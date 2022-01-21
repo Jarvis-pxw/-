2077e7f497bdc51822b5082d84754757c16027a7
Jarvis-pxw <98008847+Jarvis-pxw@users.noreply.github.com>
2022-01-21 16:09:30 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第一天
+{
+    internal class Class1
+    {static void main()
+        {
+            Console.WriteLine("Hello World");/*--打印的内容*/
+            Console.ReadKey();/*暂停*//*等待按下任意键*/
+            Console.WriteLine("我也是Hello World");
+        }
+    }
+}
