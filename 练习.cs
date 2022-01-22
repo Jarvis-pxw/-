dc43ab20a0484a8d6be45dbfcb48677e84f51eee
Jarvis-pxw <98008847+Jarvis-pxw@users.noreply.github.com>
2022-01-22 20:20:11 +0800

    Add files via upload

---
+++ 练习.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第二天
+{
+    class 联系
+    {
+        static void Main(string[] args)
+        {
+            string name = "蒲宣文";
+            int age = 19;
+            string email = "2712889535@qq.com";
+            Console.WriteLine("我叫" + name + ",我今年"+age + email);
+            Console.ReadLine();
+        }
+    }
+}
