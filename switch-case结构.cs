e175cc3c689e00ed7d02145b3060c75d85482866
Jarvis-pxw <98008847+Jarvis-pxw@users.noreply.github.com>
2022-01-24 22:18:52 +0800

    Add files via upload

---
+++ switch-case结构.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第四天
+{
+    internal class switch_case结构
+    {
+        static void Main()
+        {
+            bool b = true;
+            double salary = 5000;
+            Console.WriteLine("请输入对李四的年终评定");
+            string level=Console.ReadLine();
+            switch (level)
+            {
+                case "A":
+                    salary += 500;
+                    break;
+                case "B":
+                    salary += 200;
+                    break;
+                case "C":
+                    salary -= 200;
+                    break;
+                default:
+                    Console.WriteLine("输入有误,程序退出");
+                    b= false;
+                    break;
+            }
+            if (b)
+            {
+                Console.WriteLine("李四明年的工资是{0}元", salary);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 判断闰年.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第四天
+{
+    internal class 判断闰年
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个年份");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入一个月份");
+            int month = Convert.ToInt32(Console.ReadLine());
+            int day = 0;
+            switch(month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:day = 31;
+                    break;
+                case 2:
+                    if((year%400==0)||(year%4==0&&year%100!=0))
+                    {
+                        day = 29;
+                    }
+                    else
+                    {
+                        day = 28;
+                    }
+                    break;
+                default:day = 30;
+                    break;
+            }
+            Console.WriteLine("{0}年{1}月{2}日",year,month,day);
+            Console.ReadKey();
+        }
+    }
+}
