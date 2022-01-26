bbcc44cf86a1416afe5e18656517a6ec6be4c9e2
Jarvis-pxw <98008847+Jarvis-pxw@users.noreply.github.com>
2022-01-26 22:10:11 +0800

    Add files via upload

---
+++ 数组.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第六天
+{
+    internal class 数组
+    {
+        static void Main()
+        {
+            //string[] names = { "A", "B", "C", "D", "E" };
+            //string str = null;
+            //for (int i = 0; i < names.Length-1; i++)
+            //{
+            //    str += names[i] + "|";
+            //}
+            //Console.WriteLine(str);
+            //Console.ReadKey();
+            int[] nums = { 1, 2, 4, 5, 7, 9, 0 };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    nums[i] += 1; ;
+                }
+                else if(nums[i]<0)
+                {
+                    nums[i]-=1;
+                }
+                else
+                {
+                    nums[i] = 0;
+                }
+            }
+            for (int i = 0; i < nums.Length - 1; i++) ;
+        }
+    }
+}
