e87e2ff10c0a4a788d35ff167440d63467b20050
Jarvis-pxw <98008847+Jarvis-pxw@users.noreply.github.com>
2022-01-27 22:47:22 +0800

    Add files via upload

---
+++ 方法练习.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第七天
+{
+    internal class 方法练习
+    {
+        //static void Main()
+        //{
+
+        //}
+        //public static void IsYerOrNo(string input)
+        //{
+        //    while (true)
+        //    {
+        //        if (input == "yes"||input =="no")
+        //        {
+        //            return ;
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("只能输入yes或者no，请重新输入");
+        //            input = Console.ReadLine();
+        //        }
+            //}
+        //}
+
+}
